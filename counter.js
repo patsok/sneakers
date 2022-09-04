1586ef8ba6e1b281a8572c3d16a7846ec9e85746
Falco <patryk.sokol@gmail.com>
2022-09-04 14:31:42 +0200

    make mobile nav

--- counter.js
+++ counter.js
@@ -1,9 +1,3 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
+
 }
---
+++ pages/_sections/nav/nav.js
@@ -0,0 +1,48 @@
+const hamburgerButton = document.querySelector('.nav-hamburger > .icon-menu');
+const hamburgerCloseButton = document.querySelector('.nav-hamburger > .icon-close');
+const navMobile = document.querySelector('.nav__items--mobile');
+const navShadow = document.querySelector('.nav__shadow');
+const navMobileItems = document.querySelectorAll('.nav__items--mobile ul li');
+
+const openMenu = () => {
+    hamburgerCloseButton.classList.add('nav-hamburger__btn--is-visible');
+    hamburgerCloseButton.classList.remove('nav-hamburger__btn--is-hidden');
+    hamburgerButton.classList.remove('nav-hamburger__btn--is-visible');
+    hamburgerButton.classList.add('nav-hamburger__btn--is-hidden');
+    navMobile.classList.remove('nav__items--mobile--is-hidden');
+    navMobile.classList.add('nav__items--mobile--is-visible');
+    navShadow.classList.add('nav__shadow--is-visible');
+    navShadow.classList.remove('nav__shadow--is-hidden');
+}
+
+const closeMenu = () => {
+    hamburgerCloseButton.classList.remove('nav-hamburger__btn--is-visible');
+    hamburgerCloseButton.classList.add('nav-hamburger__btn--is-hidden');
+    hamburgerButton.classList.add('nav-hamburger__btn--is-visible');
+    hamburgerButton.classList.remove('nav-hamburger__btn--is-hidden');
+    navMobile.classList.remove('nav__items--mobile--is-visible');
+    navMobile.classList.add('nav__items--mobile--is-hidden');
+    navShadow.classList.remove('nav__shadow--is-visible');
+    navShadow.classList.add('nav__shadow--is-hidden');
+}
+
+navShadow.addEventListener('click',closeMenu);
+
+window.addEventListener('popstate',()=>{
+    const state = document.querySelector('.nav__items--mobile--is-visible');
+    console.log(state);
+    if (state !== null){
+        closeMenu();
+    }
+})
+
+navMobileItems.forEach(item => {
+    item.addEventListener('click',closeMenu);
+});
+
+
+hamburgerButton.addEventListener('click',openMenu);
+hamburgerCloseButton.addEventListener('click',closeMenu);
+
+
+
