b5f9b966259d582973fba7d7fc7100b047f81d6f
Falco <patryk.sokol@gmail.com>
2022-09-06 12:46:24 +0200

    fix arrows and gallery responsiveness

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 import './style.scss'
-// import javascriptLogo from './javascript.svg'
-
 import '@/nav/nav'
 import '@/nav/_partials/account-cartbox/account-cartbox'
 import '@/product/_partials/product-gallery/product-gallery'
 import '@/product/_partials/product-prop/product-prop'
 
-// import '@splidejs/splide/css/core';
-
-
 setTimeout(function(){
     document.body.className="";
-},500);
+},300);
--- pages/_sections/product/_partials/product-gallery/product-gallery.js
+++ pages/_sections/product/_partials/product-gallery/product-gallery.js
@@ -1,133 +1,140 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css/skyblue';
 
-var splide = new Splide("#main-slider", {
-    // width: 445,
-    height: 445,
-    pagination: false,
-    cover: true,
-    breakpoints: {
-        850: {
-            height: 300,
-        },
+
+const displayController = (() => {
+    const splideLightboxContainer = document.querySelector('.slider-lightbox');
+    const splideLightboxShadow = document.querySelector('.lightbox-shadow');
+
+    const openGallery = () => {
+        history.pushState(null, null, document.URL);
+        toggleGallery();
+
+        //initialize slide height;
+        const splideSlidesLightbox = document.querySelectorAll('#main-slider-lightbox .splide__slide');
+        splideSlidesLightbox.forEach(slide => {
+            slide.style.height = `${slide.clientWidth}px`;
+        })
     }
-});
 
-var thumbnails = document.getElementsByClassName("thumbnail");
-var current;
+    const closeGallery = () => {
+        toggleGallery();
+    }
 
-for (var i = 0; i < thumbnails.length; i++) {
-    initThumbnail(thumbnails[i], i);
-}
+    const toggleGallery = () => {
+        splideLightboxContainer.classList.toggle('is-visible');
+        splideLightboxShadow.classList.toggle('lightbox-shadow--is-visible');
+        splideLightboxShadow.classList.toggle('lightbox-shadow--is-hidden');
+    }
 
-function initThumbnail(thumbnail, index) {
-    thumbnail.addEventListener("click", function () {
-        splide.go(index);
-    });
-}
+    return { openGallery, closeGallery }
+})()
 
-splide.on("mounted move", function () {
-    var thumbnail = thumbnails[splide.index];
 
-    if (thumbnail) {
-        if (current) {
-            current.classList.remove("is-active");
+const galleryController = (() => {
+    const splideMainInitialization = (() => {
+        const splide = new Splide("#main-slider", {
+            autoWidth: true,
+            heightRatio: 1.0,
+            pagination: false,
+            cover: true,
+            breakpoints: {
+                850: {
+                    height: 300,
+                },
+            }
+        });
+
+        const thumbnails = document.querySelectorAll(".thumbnail");
+        let current;
+
+        for (let i = 0; i < thumbnails.length; i++) {
+            initThumbnail(thumbnails[i], i);
         }
 
-        thumbnail.classList.add("is-active");
-        current = thumbnail;
-    }
-});
-
-splide.mount();
-
-const splideLightboxMain = document.querySelector('#main-slider-lightbox');
-const splideThumbnailsLightboxContainer = document.querySelector('#thumbnails-lightbox');
-const splideLightboxContainer = document.querySelector('.slider-lightbox');
-const splideLightboxShadow = document.querySelector('.lightbox-shadow');
-let splideWidth = 0;
-
-const openGallery = () => {
-    history.pushState(null, null, document.URL);
-    splideLightboxContainer.classList.add('is-visible');
-    splideLightboxShadow.classList.add('lightbox-shadow--is-visible');
-    splideLightboxShadow.classList.remove('lightbox-shadow--is-hidden');
-    splideWidth = document.querySelector('#main-slider-lightbox .splide__slide').clientWidth;
-
-    const splideSlidesLightbox = document.querySelectorAll('#main-slider-lightbox .splide__slide');
-    console.log(splideSlidesLightbox);
-    splideSlidesLightbox.forEach(slide => {
-        console.log(slide.clientWidth)
-        slide.style.height = `${slide.clientWidth}px`;
-    }
-    )
-}
-
-const closeGallery = () => {
-    splideLightboxContainer.classList.remove('is-visible');
-    splideLightboxShadow.classList.remove('lightbox-shadow--is-visible');
-    splideLightboxShadow.classList.add('lightbox-shadow--is-hidden');
-}
-
-const splideSlides = document.querySelectorAll('#main-slider .splide__slide');
-
-splideSlides.forEach(slide => {
-    slide.addEventListener('click', openGallery)
-});
-
-var splideLightbox = new Splide("#main-slider-lightbox", {
-    width: 550,
-    // height: 550,
-    pagination: false,
-    cover: true,
-    heightRatio: 1.0,
-    autoHeight: true,
-    // mediaQuery: 'max',
-    //     breakpoints: {
-    // 		830: {
-    //             height: 300,
-    // 		},
-    //   }
-});
-
-var thumbnailsLightbox = document.getElementsByClassName("thumbnail-lightbox");
-var currentLightbox;
-
-for (var i = 0; i < thumbnailsLightbox.length; i++) {
-    initThumbnailLightbox(thumbnailsLightbox[i], i);
-}
-
-function initThumbnailLightbox(thumbnailLightbox, index) {
-    thumbnailLightbox.addEventListener("click", function () {
-        splideLightbox.go(index);
-    });
-}
-
-splideLightbox.on("mounted move", function () {
-    var thumbnailLightbox = thumbnailsLightbox[splideLightbox.index];
-
-    if (thumbnailLightbox) {
-        if (currentLightbox) {
-            currentLightbox.classList.remove("lightbox-is-active");
+        function initThumbnail(thumbnail, index) {
+            thumbnail.addEventListener("click", function () {
+                splide.go(index);
+            });
         }
 
-        thumbnailLightbox.classList.add("lightbox-is-active");
-        currentLightbox = thumbnailLightbox;
-    }
-});
+        splide.on("mounted move", function () {
+            var thumbnail = thumbnails[splide.index];
 
-splideLightbox.mount();
+            if (thumbnail) {
+                if (current) {
+                    current.classList.remove("is-active");
+                }
 
-const lightboxCloseButton = document.querySelector('.slider-lightbox .font-icon-close');
+                thumbnail.classList.add("is-active");
+                current = thumbnail;
+            }
+        });
 
-lightboxCloseButton.addEventListener('click', closeGallery);
+        splide.mount();
+    })()
 
-window.addEventListener('popstate', () => {
-    const state = document.querySelector('.lightbox-shadow--is-visible');
-    if (state !== null) {
-        closeGallery();
-    }
-})
+    const splideLightboxInitialization = (() => {
+        const splideLightbox = new Splide("#main-slider-lightbox", {
+            width: 550,
+            pagination: false,
+            cover: true,
+            heightRatio: 1.0,
+            autoHeight: true,
+        });
 
-splideLightboxShadow.addEventListener('click', closeGallery);
+        const thumbnailsLightbox = document.getElementsByClassName("thumbnail-lightbox");
+        let currentLightbox;
+
+        for (let i = 0; i < thumbnailsLightbox.length; i++) {
+            initThumbnailLightbox(thumbnailsLightbox[i], i);
+        }
+
+        function initThumbnailLightbox(thumbnailLightbox, index) {
+            thumbnailLightbox.addEventListener("click", function () {
+                splideLightbox.go(index);
+            });
+        }
+
+        splideLightbox.on("mounted move", function () {
+            var thumbnailLightbox = thumbnailsLightbox[splideLightbox.index];
+
+            if (thumbnailLightbox) {
+                if (currentLightbox) {
+                    currentLightbox.classList.remove("lightbox-is-active");
+                }
+
+                thumbnailLightbox.classList.add("lightbox-is-active");
+                currentLightbox = thumbnailLightbox;
+            }
+        });
+
+        splideLightbox.mount();
+    })()
+
+    //back button (swipe back) on browsers closes gallery
+    window.addEventListener('popstate', () => {
+        const state = document.querySelector('.lightbox-shadow--is-visible');
+        if (state !== null) {
+            displayController.closeGallery();
+        }
+    })
+
+    //open lightbox gallery after clicking on main photo
+    const splideSlides = document.querySelectorAll('#main-slider .splide__slide');
+    splideSlides.forEach(slide => {
+        slide.addEventListener('click', displayController.openGallery)
+    });
+
+    //gallery close button
+    const lightboxCloseButton = document.querySelector('.slider-lightbox .font-icon-close');
+    lightboxCloseButton.addEventListener('click', displayController.closeGallery);
+
+    //close gallery on outside click
+    const splideLightboxShadow = document.querySelector('.lightbox-shadow');
+    splideLightboxShadow.addEventListener('click', displayController.closeGallery);
+
+})()
+
+
 
